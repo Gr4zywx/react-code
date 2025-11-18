75e70270e677ed64e8c8147999e74a38c65e6e32
GRAZIELLY PERREIRA RODRIGUES <58072783807@info.local>
2025-11-18 17:26:21 -0300

    implementação da busca de produtos

--- src/pages/Produtos/Produtos.tsx
+++ src/pages/Produtos/Produtos.tsx
@@ -1,31 +1,38 @@
 import './Produtos.css';
-import banner_1 from '../../assets/imgs/banner.png';
-import banner_2 from '../../assets/imgs/banner2.png';
-import banner_3 from '../../assets/imgs/banner3.png';
-import choc_belga from '../../assets/imgs/choc-belga.png';
-import choc_ninho from '../../assets/imgs/choc-ninho.png';
-import cenoura_choc from '../../assets/imgs/cenoura-choc.png';
-import ninho_morango from '../../assets/imgs/choc-ninho-morango.png';
-import choc_pistache from '../../assets/imgs/choc-pistache.png';
-import choc_oreo from '../../assets/imgs/choc-oreo.png';
+
 import whatsapp from '../../assets/whatsapp.png';
 import { useEffect, useState } from 'react';
 import type { Bolo } from '../../types/Bolo';
 import { getBolos } from '../../services/bolosServices';
 import CardProduto from '../../components/CardProduto/CardProduto';
 import Carrossel from '../../components/Carrossel/Carrossel';
+import Header from '../../components/Header/Header';
+import { useLocation } from 'react-router-dom';
 
 
-//funções assincronas
+
 export default function Produtos() {
 
     const [bolos, setBolos] = useState<Bolo[]>([]);
+    const location = useLocation();
+
+    const parametrosPesquisados = new URLSearchParams(location.search);
+    const termo_pesquisado = parametrosPesquisados.get('query');
 
     const fetchBolos = async () => {
         try {
             const dados = await getBolos();
-            console.log("Dados retornados da API", dados);
-            setBolos(dados);
+            if (termo_pesquisado) {
+                const dados_filtrados = dados.filter
+                    (b => b.nome.toLowerCase().includes(termo_pesquisado.toLowerCase()) ||
+                        b.descricao.toLowerCase().includes(termo_pesquisado.toLocaleLowerCase()) ||
+                        b.caterosias.some(cat => cat.toLowerCase().includes(termo_pesquisado.toLowerCase()))
+                    )
+                    setBolos(dados_filtrados)
+            }else{
+                console.log("Dados retornados da API", dados);
+                setBolos(dados);
+            }
         } catch (error) {
             console.error("Erro ao executar getBolos: ", error);
         }
@@ -33,40 +40,49 @@ export default function Produtos() {
 
     useEffect(() => {
         fetchBolos();
+        console.log("Termo pesquisado: ", termo_pesquisado);
     }, []);
 
 
     return (
+        <>
+            <Header />
+            <main>
+                <Carrossel />
+                <section className="container_produtos">
+                    <h1 className="acessivel">produtos de chocolate</h1>
+                    <div className="titulo">
+                        <span>
+                            {
+                                termo_pesquisado ? `Resultados para: ${termo_pesquisado}` :
+                                "Nome da categoria"
+                            }
+                        </span>
+                        <hr />
+                    </div>
 
-        <main>
-            <Carrossel />
-            <section className="container_produtos">
-                <h1 className="acessivel">produtos de chocolate</h1>
-                <div className="titulo">
-                    <span>Chocolate</span>
-                    <hr />
-                </div>
+                    <section className="cards">
 
-                <section className="cards">
+                        {
+                            bolos.map((b: Bolo) => (
+                                <CardProduto nome={b.nome}
+                                    descricao={b.descricao}
+                                    preco={b.preco}
+                                    imagem={b.imagens[0] ?? ""}
+                                    peso={b.peso}
+                                />
+                            ))
+                        }
 
-                    {
-                        bolos.map((b: Bolo) => (
-                            <CardProduto nome={b.nome}
-                                descricao={b.descricao}
-                                preco={b.preco}
-                                imagem={b.imagens[0] ?? ""}
-                                peso={b.peso}
-                            />
-                        ))
-                    }
-
+                    </section>
                 </section>
-            </section>
 
-            <a className="whatsapp" href="https://wa.me/5511999999999?text=Olá%20,%20gostaria%20de%20mais%20informações."
-                target="_blank">
-                <img src={whatsapp} alt="icone do whatsapp" />
-            </a>
-        </main>
+                <a className="whatsapp" href="https://wa.me/5511999999999?text=Olá%20,%20gostaria%20de%20mais%20informações."
+                    target="_blank">
+                    <img src={whatsapp} alt="icone do whatsapp" />
+                </a>
+            </main >
+
+        </>
     )
 }
